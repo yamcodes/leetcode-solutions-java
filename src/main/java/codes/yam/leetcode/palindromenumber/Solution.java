f4c829a61a07d0ddfa34a0eae242f054a1c4d04f
Yam C Borodetsky <yam@yam.codes>
2026-02-06 23:56:29 +0500

    Add new files

---
+++ src/main/java/codes/yam/leetcode/palindromenumber/Solution.java
@@ -0,0 +1,22 @@
+package codes.yam.leetcode.palindromenumber;
+
+
+/**
+ * <p>Solution for the <b>Palindrome Number</b> problem.</p>
+ * <ul>
+ * <li><b>Time Complexity:</b> <code>O(log10(n))</code></li>
+ * <li><b>Space Complexity:</b> <code>O(1)</code></li>
+ * </ul>
+ */
+class Solution {
+    /**
+     * Checks if an integer is a palindrome.
+     *
+     * @param x The integer to check.
+     * @return true if x is a palindrome, false otherwise.
+     */
+    public boolean isPalindrome(int x) {
+        IO.println("Hello");
+        return true;
+    }
+}
---
+++ src/main/java/codes/yam/leetcode/palindromenumber/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>Solutions for the "Palindrome Number" problem on LeetCode.</p>
+ * <ul>
+ * <li><b>Slug:</b> <code>palindrome-number</code></li>
+ * <li><b>Difficulty:</b> Easy</li>
+ * </ul>
+ *
+ * @see <a href="https://leetcode.com/problems/palindrome-number/">Problem Link</a>
+ */
+package codes.yam.leetcode.palindromenumber;
