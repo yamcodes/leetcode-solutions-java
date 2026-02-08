896df84cedbd3d6b28d5efa7578af3b0bf856302
Yam Borodetsky <yam@yam.codes>
2026-02-09 02:41:24 +0500

    Implement minimum deletions to make string balanced logic

--- src/main/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/Solution.java
+++ src/main/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/Solution.java
@@ -1,7 +1,35 @@
 package codes.yam.leetcode.minimumdeletionstomakestringbalanced;
 
+
 class Solution {
+  /**
+   * Counts the number of B's before and A's after the given index.
+   *
+   * @param s     the string
+   * @param index the split point index
+   * @return [B count before index, A count after index]
+   */
+  int[] getSliceInfo(String s, int index) {
+    int aCount = 0, bCount = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == 'b' && i < index) {
+        bCount++;
+      }
+      if (c == 'a' && i > index) {
+        aCount++;
+      }
+    }
+    return new int[]{bCount, aCount};
+  }
+
   int minimumDeletions(String s) {
-    return 0;
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < s.length(); i++) {
+      int[] info = getSliceInfo(s, i);
+      int computed = info[0] + info[1];
+      min = Math.min(min, computed);
+    }
+    return min;
   }
 }
