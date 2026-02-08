cd7e9edacb0fd7454fa9a69463c2dfa8b867a73e
Yam Borodetsky <yam@yam.codes>
2026-02-09 02:43:29 +0500

    Add test cases for minimum deletions to make string balanced

--- src/test/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/SolutionTest.java
+++ src/test/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/SolutionTest.java
@@ -1,18 +1,24 @@
 package codes.yam.leetcode.minimumdeletionstomakestringbalanced;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class SolutionTest {
   static Stream<Arguments> cases() {
     return Stream.of(
         arguments("aababbab", 2),
-        arguments("bbaaaaabb", 2));
+            arguments("bbaaaaabb", 2),
+            arguments("b", 0),
+            arguments("ba", 1),
+            arguments("aabbbaa", 3),
+            arguments("abababab", 3),
+            arguments("bbbbaaaa", 0));
   }
 
   @ParameterizedTest
