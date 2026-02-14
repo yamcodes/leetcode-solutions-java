9fc6b12d5d8e46e9ba8b8e541fa3021890ae52d7
Yam Borodetsky <yam@yam.codes>
2026-02-14 22:17:04 +0500

    Initialize memoization array as a class member

--- src/main/java/codes/yam/leetcode/climbingstairs/SolutionMemoized.java
+++ src/main/java/codes/yam/leetcode/climbingstairs/SolutionMemoized.java
@@ -9,18 +9,19 @@ package codes.yam.leetcode.climbingstairs;
  * </ul>
  */
 class SolutionMemoized {
+  int[] memo;
+
   int rec(int n) {
-    int[] memo = new int[n + 1];
-    memo[1] = 1;
-    memo[2] = 2;
-    if (memo[n - 1] == 0) memo[n - 1] = rec(n - 1);
-    if (memo[n - 2] == 0) memo[n - 2] = rec(n - 2);
-    return memo[n - 1] + memo[n - 2];
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (memo[n] == 0) {
+      memo[n] = rec(n - 1) + rec(n - 2);
+    }
+    return memo[n];
   }
 
   int climbStairs(int n) {
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+    memo = new int[n + 1];
     return rec(n);
   }
 }
