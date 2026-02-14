4d737f4bc0062e1e72fbf475ebc7652950e4a587
Yam Borodetsky <yam@yam.codes>
2026-02-14 21:33:02 +0500

    Add TODO to refactor Solution with cleaner DP formulation

--- src/main/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/package-info.java
+++ src/main/java/codes/yam/leetcode/minimumdeletionstomakestringbalanced/package-info.java
@@ -26,6 +26,10 @@
  *   <li>{@code Solution} — Space-optimized DP, <code>O(n)</code> time, <code>O(1)</code> space
  * </ol>
  *
+ * <p><b>TODO:</b> Current {@code Solution} uses a split-point-derived approach. Once the DP
+ * progression is implemented, replace it with the cleaner DP formulation (rename current to {@code
+ * SolutionSplitPointOptimized}).
+ *
  * @see <a href="https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/">Problem
  *     Link</a>
  */
