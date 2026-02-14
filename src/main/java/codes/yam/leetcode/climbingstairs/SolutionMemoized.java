d910277e76806a408187e68c7e217d87d679ad61
Yam Borodetsky <yam@yam.codes>
2026-02-14 22:12:31 +0500

    Implement memoized solution for climbing stairs problem

--- src/main/java/codes/yam/leetcode/climbingstairs/SolutionMemoized.java
+++ src/main/java/codes/yam/leetcode/climbingstairs/SolutionMemoized.java
@@ -9,7 +9,16 @@ package codes.yam.leetcode.climbingstairs;
  * </ul>
  */
 class SolutionMemoized {
+  int rec(int n) {
+    int[] memo = new int[n + 1];
+    memo[1] = 1;
+    memo[2] = 2;
+    if (memo[n - 1] == 0) memo[n - 1] = rec(n - 1);
+    if (memo[n - 2] == 0) memo[n - 2] = rec(n - 2);
+    return memo[n - 1] + memo[n - 2];
+  }
+
   int climbStairs(int n) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    return rec(n);
   }
 }
