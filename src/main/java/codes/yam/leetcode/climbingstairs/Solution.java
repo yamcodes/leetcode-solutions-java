5de63dd70d0d8ccbc84bb68fd59d95226c8bd8aa
Yam Borodetsky <yam@yam.codes>
2026-02-14 23:42:04 +0500

    Make Solution class public and adjust loop start index

--- src/main/java/codes/yam/leetcode/climbingstairs/Solution.java
+++ src/main/java/codes/yam/leetcode/climbingstairs/Solution.java
@@ -1,13 +1,13 @@
 package codes.yam.leetcode.climbingstairs;
 
-class Solution {
+public class Solution {
   int climbStairs(int n) {
     if (n == 1) return 1;
     if (n == 2) return 2;
     int a = 1;
     int b = 2;
     int c = a + b;
-    for (int i = 4; i <= n; i++) {
+    for (int i = 3; i <= n; i++) {
       c = a + b;
       a = b;
       b = c;
