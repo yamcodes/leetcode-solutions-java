20c61781973108251c45692c6dbf0c609c3daeb9
Yam Borodetsky <yam@yam.codes>
2026-02-19 00:47:34 +0500

    Optimize DP initialization and transition for coin change

--- src/main/java/codes/yam/leetcode/coinchange/SolutionDp.java
+++ src/main/java/codes/yam/leetcode/coinchange/SolutionDp.java
@@ -1,16 +1,21 @@
 package codes.yam.leetcode.coinchange;
 
+import java.util.Arrays;
+
 class SolutionDp {
   int coinChange(int[] coins, int amount) {
     int[] dp = new int[amount + 1];
+    Arrays.fill(dp, 1, amount + 1, amount + 1);
     for (int a = 1; a <= amount; a++) {
-      int min = amount + 1;
       for (int c : coins) {
-        if (c > a) continue;
-        min = Math.min(min, 1 + dp[a - c]);
+        if (c <= a) {
+          dp[a] = Math.min(dp[a], 1 + dp[a - c]);
+        }
       }
-      dp[a] = min;
     }
-    return dp[amount] > amount ? -1 : dp[amount];
+    if (dp[amount] > amount) {
+      return -1;
+    }
+    return dp[amount];
   }
 }
